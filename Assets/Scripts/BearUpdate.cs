6dd3a350aa708dc6cd2a46f08a0ffda484531976
mwojnar <michael.j.wojnar@gmail.com>
2015-01-24 16:52:23 -0600

    Changed bear animation speed, created first bear AI, added human attack animation.

--- Assets/Scripts/BearUpdate.cs
+++ Assets/Scripts/BearUpdate.cs
@@ -2,20 +2,47 @@
 using System.Collections;
 
 public class BearUpdate : MonoBehaviour {
-	
+
+	public float speed = 20, patrolArea = 400;
 	private Animator _animator;
+	private bool moveLeft = true;
+	private float startX, startY;
 
 	// Use this for initialization
 	void Awake () {
 		
 		_animator = GetComponent<Animator>();
+		startX = this.transform.position.x;
+		startY = this.transform.position.y;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		_animator.Play (Animator.StringToHash ("Run"));
+
+		if (moveLeft) {
+
+			this.transform.position = new Vector3(this.transform.position.x - speed, this.transform.position.y, this.transform.position.z);
+			if (startX - this.transform.position.x > patrolArea / 2) {
+
+				moveLeft = false;
+				if( transform.localScale.x > 0f )
+					transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
+
+			}
+
+		} else {
+
+			this.transform.position = new Vector3(this.transform.position.x + speed, this.transform.position.y, this.transform.position.z);
+			if (startX - this.transform.position.x < -patrolArea / 2) {
+				
+				moveLeft = true;
+				if( transform.localScale.x < 0f )
+					transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
+				
+			}
+
+		}
 
 	}
 }
