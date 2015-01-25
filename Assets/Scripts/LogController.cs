36eaaed176966a041de8489a53998d42972c8fdd
mwojnar <michael.j.wojnar@gmail.com>
2015-01-24 20:38:00 -0600

    Added logs and stuff.

---
+++ Assets/Scripts/LogController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogController : MonoBehaviour {
+
+	public static GameObject[] logs;
+	private static int logsLeft = 3;
+
+	public static void destroyLog() {
+		
+		logsLeft--;
+		(logs [logsLeft].GetComponent<Animator> ()).speed = 1;
+
+	}
+
+}
